ecff001ffe53edd1c5402e6faaf4f5f04fea21b6
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2018-10-04 21:21:20 +0530

    Change vulnerable dependency version for flask

--- server/app.py
+++ server/app.py
@@ -8,83 +8,82 @@ from settings import DEBUG
 from constants import GITHUB_REPOSITORY_LINK, DEFAULT_THRESHOLD, THRESHOLD_DELTA
 from utils import update_dict, check_prediction
 
-
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/update-user", methods=['GET'])
 def main():
-	# get data from args
-	gcm_id = request.args.get("gcm_id")
-	new_data = request.args.get("user_data")
+    # get data from args
+    gcm_id = request.args.get("gcm_id")
+    new_data = request.args.get("user_data")
 
-	file_path = "user_data/" + gcm_id
+    file_path = "user_data/" + gcm_id
 
-	if os.path.isfile(file_path):
-		user_history = pickle.load(open(file_path, "rb"))
+    if os.path.isfile(file_path):
+        user_history = pickle.load(open(file_path, "rb"))
 
-		existing_data = user_history['data']
-		threshold = user_history['threshold']
+        existing_data = user_history['data']
+        threshold = user_history['threshold']
 
-		data_dict = update_dict(existing_data, new_data)
-	else:
-		data_dict = update_dict({}, new_data)
-		threshold = DEFAULT_THRESHOLD
+        data_dict = update_dict(existing_data, new_data)
+    else:
+        data_dict = update_dict({}, new_data)
+        threshold = DEFAULT_THRESHOLD
 
-	data_dict = check_prediction(data_dict, threshold)
+    data_dict = check_prediction(data_dict, threshold)
 
-	user_json = {
-		'data' : data_dict,
-		'threshold' : threshold,
-	}
-	
-	pickle.dump(user_json, open(file_path, "wb"))
-	
-	return jsonify(**data_dict)
+    user_json = {
+        'data': data_dict,
+        'threshold': threshold,
+    }
+
+    pickle.dump(user_json, open(file_path, "wb"))
+
+    return jsonify(**data_dict)
 
 
 @app.route("/change-threshold", methods=['GET'])
 def change_threshold():
-	gcm_id = request.args.get("gcm_id")
-	delta_flag = request.args.get("flag")
+    gcm_id = request.args.get("gcm_id")
+    delta_flag = request.args.get("flag")
 
-	file_path = "user_data/" + gcm_id
+    file_path = "user_data/" + gcm_id
 
-	if os.path.isfile(file_path):
-		user_history = pickle.load(open(file_path, "rb"))
+    if os.path.isfile(file_path):
+        user_history = pickle.load(open(file_path, "rb"))
 
-		existing_data = user_history['data']
-		threshold = user_history['threshold']
+        existing_data = user_history['data']
+        threshold = user_history['threshold']
 
-		if delta_flag == 0:
-			new_threshold = threshold - THRESHOLD_DELTA
-		else:
-			new_threshold = threshold + THRESHOLD_DELTA
+        if delta_flag == 0:
+            new_threshold = threshold - THRESHOLD_DELTA
+        else:
+            new_threshold = threshold + THRESHOLD_DELTA
 
-		user_json = {
-			'data' : existing_data,
-			'threshold' : new_threshold,
-		}
-		
-		pickle.dump(user_json, open(file_path, "wb"))
+        user_json = {
+            'data': existing_data,
+            'threshold': new_threshold,
+        }
 
-		return True
+        pickle.dump(user_json, open(file_path, "wb"))
 
-	return False
+        return True
+
+    return False
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-	message = "Invalid URL, Please refer to the docs for correct URL"
+    message = "Invalid URL, Please refer to the docs for correct URL"
 
-	response = {
-		'message' : message,
-		'docs_url' : GITHUB_REPOSITORY_LINK,
-	}
+    response = {
+        'message': message,
+        'docs_url': GITHUB_REPOSITORY_LINK,
+    }
 
-	return jsonify(**response)
+    return jsonify(**response)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- server/utils.py
+++ server/utils.py
@@ -1,13 +1,13 @@
 import operator
+
 import enchant
 import nltk
-from nltk.stem.wordnet import WordNetLemmatizer
-import requests
 from gcm import *
+from nltk.stem.wordnet import WordNetLemmatizer
 
-from settings import DEBUG
-from constants import PROPER_NOUN_POS_TAGS, DEFAULT_THRESHOLD
 from api_constants import WALMART_OPEN_PRODUCT_API_KEY, GCM_API_KEY
+from constants import PROPER_NOUN_POS_TAGS
+from settings import DEBUG
 
 # append custom path for nltk corpus
 nltk.data.path.append("nltk_data/")
@@ -17,67 +17,66 @@ enchant_dictionary = enchant.Dict("en_US")
 
 
 def check_candidature(query_string):
-	walmart_url = "http://api.walmartlabs.com/v1/search?apiKey={0}&query={1}".format(WALMART_OPEN_PRODUCT_API_KEY, query_string)
-	response = requests.get(walmart_url)
-	if response.ok:
-		if response.json()['totalResults'] > 0:
-			return True
+    walmart_url = "http://api.walmartlabs.com/v1/search?apiKey={0}&query={1}".format(WALMART_OPEN_PRODUCT_API_KEY,
+                                                                                     query_string)
+    response = requests.get(walmart_url)
+    if response.ok:
+        if response.json()['totalResults'] > 0:
+            return True
 
-	return False
+    return False
 
 
 def update_dict(existing_dict, unclean_text):
-	tokens = nltk.word_tokenize(unclean_text)
-	pos_tag_data = nltk.pos_tag(tokens)
+    tokens = nltk.word_tokenize(unclean_text)
+    pos_tag_data = nltk.pos_tag(tokens)
 
-	for token_tuple in pos_tag_data:
-		noun_word = token_tuple[0]
-		clean_noun_word = ''.join(e for e in noun_word if e.isalnum())
+    for token_tuple in pos_tag_data:
+        noun_word = token_tuple[0]
+        clean_noun_word = ''.join(e for e in noun_word if e.isalnum())
 
-		# check if the "clean_noun_word" is not an empty string
-		if clean_noun_word:
-			if token_tuple[1] in PROPER_NOUN_POS_TAGS and enchant_dictionary.check(clean_noun_word):
-				lemmatized_word = lmtzr.lemmatize(clean_noun_word)
-				
-				if len(lemmatized_word) > 2:
-					existing_dict = increase_score(existing_dict, lemmatized_word, 1)
+        # check if the "clean_noun_word" is not an empty string
+        if clean_noun_word:
+            if token_tuple[1] in PROPER_NOUN_POS_TAGS and enchant_dictionary.check(clean_noun_word):
+                lemmatized_word = lmtzr.lemmatize(clean_noun_word)
 
+                if len(lemmatized_word) > 2:
+                    existing_dict = increase_score(existing_dict, lemmatized_word, 1)
 
-	return existing_dict
+    return existing_dict
+
 
 def increase_score(score_dict, key, value):
-	lowercase_key = key.lower()
-	if lowercase_key in score_dict:
-		score_dict[lowercase_key] += value
-	else:
-		score_dict[lowercase_key] = value
+    lowercase_key = key.lower()
+    if lowercase_key in score_dict:
+        score_dict[lowercase_key] += value
+    else:
+        score_dict[lowercase_key] = value
 
-	return score_dict
+    return score_dict
 
 
 def check_prediction(dictionary, threshold, gcm_device_id):
-	sum_of_freq = sum(dictionary.values())
-	max_tuple = max(dictionary.iteritems(), key=operator.itemgetter(1))
+    sum_of_freq = sum(dictionary.values())
+    max_tuple = max(dictionary.iteritems(), key=operator.itemgetter(1))
 
-	percentage_freq = (max_tuple[1]) / sum_of_freq
-	if percentage_freq >= threshold:
-		candidate_string = max_tuple[0]
-		dictionary.pop(candidate_string, None)
+    percentage_freq = (max_tuple[1]) / sum_of_freq
+    if percentage_freq >= threshold:
+        candidate_string = max_tuple[0]
+        dictionary.pop(candidate_string, None)
 
-		if check_candidature(candidate_string):
-			# send GCM message
-			if DEBUG:
-				print "{0} : successful candidate".format(candidate_string)
-			else:
-				gcm_send(gcm_device_id, candidate_string)
-			
+        if check_candidature(candidate_string):
+            # send GCM message
+            if DEBUG:
+                print("{0} : successful candidate".format(candidate_string))
+            else:
+                gcm_send(gcm_device_id, candidate_string)
 
-	return dictionary
+    return dictionary
 
 
 def gcm_send(gcm_device_id, string):
-	gcm = GCM(GCM_API_KEY)
-	data = {'search_query': string}
+    gcm = GCM(GCM_API_KEY)
+    data = {'search_query': string}
 
-	gcm.plaintext_request(registration_id=gcm_device_id, data=data)
-
+    gcm.plaintext_request(registration_id=gcm_device_id, data=data)
